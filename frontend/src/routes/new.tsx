988956125326841983e303b1bb406eb2e95ae532
ophum <inagaki0106@gmail.com>
2023-08-05 19:46:44 +0900

    few change

--- frontend/src/routes/new.tsx
+++ frontend/src/routes/new.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormLabel, Input, Radio, RadioGroup } from "@mui/joy";
+import { Button, Card, CardActions, CardContent, FormControl, FormLabel, Input, Radio, RadioGroup } from "@mui/joy";
 import { Controller, useForm } from "react-hook-form";
 import { useActionData, useSubmit } from "react-router-dom";
 
@@ -9,7 +9,7 @@ interface NewFormInput {
 }
 
 export default function New() {
-    const res = useActionData() as {status: number};
+    const res = useActionData() as { status: number };
     const form = useForm<NewFormInput>({
         defaultValues: {
             type: "send"
@@ -17,35 +17,39 @@ export default function New() {
     });
     const submit = useSubmit();
     const onSubmit = (data: NewFormInput) => {
-        console.log({data})
+        console.log({ data })
         submit({
             body: JSON.stringify(data),
-        }, {method: "post", action: "/new"})
+        }, { method: "post", action: "/new" })
     };
-    return (<div>
-        {res && (<p>{res.status}</p>)}
+    return (<Card sx={{ width: "480px" }}>
         <form onSubmit={form.handleSubmit(onSubmit)} method="post">
-            <FormControl>
-                <FormLabel>日付</FormLabel>
-                <Input type="date" {...form.register("date", {
-                    valueAsDate: true
-                })} />
-            </FormControl>
-            <FormControl>
-                <Controller name="type" control={form.control} render={({ field }) => (
-                    <RadioGroup {...field}>
-                        <Radio label="入金" value="received" />
-                        <Radio label="出金" value="send" />
-                    </RadioGroup>
-                )} />
-            </FormControl>
-            <FormControl>
-                <FormLabel>金額</FormLabel>
-                <Input type="number" endDecorator="円" {...form.register("amount", {
-                    valueAsNumber: true,
-                })} />
-            </FormControl>
-            <Button type="submit">追加</Button>
+            <CardContent>
+                {res && (<p>{res.status}</p>)}
+                <FormControl>
+                    <FormLabel>日付</FormLabel>
+                    <Input type="date" {...form.register("date", {
+                        valueAsDate: true
+                    })} />
+                </FormControl>
+                <FormControl>
+                    <Controller name="type" control={form.control} render={({ field }) => (
+                        <RadioGroup {...field}>
+                            <Radio label="入金" value="received" />
+                            <Radio label="出金" value="send" />
+                        </RadioGroup>
+                    )} />
+                </FormControl>
+                <FormControl>
+                    <FormLabel>金額</FormLabel>
+                    <Input type="number" endDecorator="円" {...form.register("amount", {
+                        valueAsNumber: true,
+                    })} />
+                </FormControl>
+            </CardContent>
+            <CardActions>
+                <Button type="submit">追加</Button>
+            </CardActions>
         </form>
-    </div>)
+    </Card>)
 }
